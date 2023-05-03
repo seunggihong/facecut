7c9f7b8f32f9fa6659ce552c3ece45a7c4b3e066
onggi <hsk1533@naver.com>
2023-05-03 11:22:11 +0900

    img naming

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import os
 from tqdm import trange
 
 # cuttin imgs tracking face
-def cut_imgs(img) :
+def cut_imgs(img, cnt) :
     face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5)
-
+    
     for (x, y, w, h) in faces:
         face = img[y:y+h, x:x+w]
-        cv2.imwrite("result/test01.jpg", face)
+        cv2.imwrite("result/result{}.jpg".format(str(cnt)), face)
 
 dirListing = os.listdir("imgs")
 
@@ -18,4 +18,4 @@ if dirListing == 0 :
 else :
     for x in trange(len(dirListing)) :
         img = cv2.imread("imgs/test{}.jpg".format(str(x+1)))
-        cut_imgs(img)
+        cut_imgs(img, x)
