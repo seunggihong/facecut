81fc3c71da30ea089c7140fc704bc80771bc0c52
onggi <hsk1533@naver.com>
2023-05-04 13:04:50 +0900

    error find

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from distutils.log import ERROR
 import cv2
 import os
 from tqdm import trange
@@ -7,15 +8,27 @@ def cut_imgs(img, cnt) :
     face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5)
     
-    for (x, y, w, h) in faces:
-        face = img[y:y+h, x:x+w]
-        cv2.imwrite("result/result{}.jpg".format(str(cnt)), face)
+    if isinstance(faces, tuple) :
+        return False
+    else :
+        for (x, y, w, h) in faces:
+            face = img[y:y+h, x:x+w]
+            cv2.imwrite("result/result{}.jpg".format(str(cnt+1)), face)
+            return True
 
 dirListing = os.listdir("imgs")
+done = 0
+err = 0
 
 if dirListing == 0 :
     print("Empty dir.")
 else :
     for x in trange(len(dirListing)) :
         img = cv2.imread("imgs/test{}.jpg".format(str(x+1)))
-        cut_imgs(img, x)
+        
+        if cut_imgs(img, x) :
+            done += 1
+        else :
+            err += 1
+            
+print("Done : {} Error : {}".format(done, err))
