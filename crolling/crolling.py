8f176f415dc37d38cca7fbfd4fb43ccc618f135c
onggi <hsk1533@naver.com>
2023-05-09 10:13:24 +0900

    dir restore

--- crolling/crolling.py
+++ crolling/crolling.py
@@ -12,14 +12,14 @@ driver = webdriver.Chrome(r'/Users/ki/Desktop/crolling/chromedriver')
 driver.get("https://images.google.com/")
 elem = driver.find_element_by_name('q')
 # search
-elem.send_keys("수지")
+elem.send_keys("강호동")
 elem.send_keys(Keys.RETURN)
 
 scroll_cnt = 3
 last_height = driver.execute_script("return document.body.scrollHeight")
 while scroll_cnt != 0 :
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(1)
+    time.sleep(3)
     new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == last_height :
         try :
@@ -31,11 +31,11 @@ while scroll_cnt != 0 :
 
 imgs = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
 count = 1
-for img in imgs :        
+for img in imgs :
     img.click()
-    time.sleep(3)
+    time.sleep(2)
     imgUrl = driver.find_element_by_css_selector(".r48jcc").get_attribute("src")
-    urllib.request.urlretrieve(imgUrl,"suzi/{}.jpg".format(count))
+    urllib.request.urlretrieve(imgUrl,"crollimg/{}.jpg".format(count))
     count += 1
-    
+
 driver.close()
